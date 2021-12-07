2a8ee3a0e2a8092e33e45428459870ea9410fdee
Maximiliano Becerra <mba@geosys.com>
2021-12-07 16:29:30 +0100

    init project

---
+++ CosmosDB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CosmosDB
+{
+    public class Program
+    {
+        // https://docs.microsoft.com/en-us/learn/modules/work-with-cosmos-db/3-exercise-work-cosmos-db-dotnet
+        private static readonly string EndpointUrl = "";
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ CosmosDB/README.md
@@ -0,0 +1,4 @@
+az login  
+az group create --location francecentral --name az204-cosmos-rg  
+az cosmosdb create --name learn-cosmos-account --resource-group az204-cosmos-rg
+
